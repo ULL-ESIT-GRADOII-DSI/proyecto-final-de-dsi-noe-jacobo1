86ebce7fb6d0ae959f781a95496bf2b0ed5c479a
alu0100622492 <alu0100622492@ull.edu.es>
2016-05-20 02:18:24 +0000

    Añadido esqema buscar usuario mongo

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var express = require('express');
 const app = express();
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -46,19 +47,37 @@ app.get('/new_user', (request, response) => {//midelware para la creacion de un 
 
 
 app.param('usuario',function(request,response,next,name){ //param (guardar dato en el midelware)
-    console.log("usuario param"+ name);
+    console.log("usuario param: "+ name);
     if (name.match(/^[a-z0-9_]*$/i)) { 
-      request.ejemplo = name;
-      console.log("request"+ request.ejemplo);
-    } else { 
+      request.user = name;
+      console.log("request: "+ request.ejemplo);
+    } else {  
       next(new Error(`<${name}> does not match 'usuario' requirements`));
     }
     next();
 });
 
 app.get('/buscar/:usuario',function(request,response){//Para llegar aqi primero tiene q haber un nombre en la etiqueta name_user
-    console.log("Buscamos usuario midelware->"+request.ejemplo);
-    
+    console.log("Midelware Buscamos usuario ->"+request.user);
+     mongodb.User.find({name: request.user},function(err,docs){
+         
+           if(docs.length > 0){
+                //console.log("Error:"+err);
+                console.log("Id de usuario:"+docs[0]._id);
+                /*var id = new ObjectId(data[0]._id);
+                console.log("Id:"+id._id);*/
+                const id = mongoose.Types.ObjectId(docs[0]._id);
+                console.log("Id:"+id);
+                mongodb.Acumulador.find({_creator: id},function(err,docs_acum){
+                    if(err) {
+                        console.error("Se ha producido un error->"+err);
+                    } else {
+                        console.log("  Acumulador:"+docs_acum);    
+                    }
+                   // response.send({contenido: data_tablas, usuario_propietario: id, mensaje_respuesta: "Busqueda realizada correctamente.", mensaje_guardado: " "});
+                });
+           }
+     });
 });
 
 app.listen(app.get('port'), () => {
